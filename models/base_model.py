48a0c483b0b7a4fc12bc1de0fc40208841f06475
ohendarko <ohendarko@gmail.com>
2023-12-06 14:00:11 +0000

    Task3 done.

---
+++ models/base_model.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+from datetime import datetime
+"""
+This contains a class BaseModel that
+defines all common attributes/methods for other classes
+"""
+class BaseModel():
+    """
+    Defines all common attributes/methods
+    for other classes
+    """
+    def __init__(self, id, created_at, updated_at):
+        self.id = id
+        self.created_at = created_at
+        self.updated_at = updated_at
+    def __str__(self):
+        """print: [<class name>] (<self.id>) <self.__dict__>"""
+        class_name = self.__class__.__name__
+        return f"[{class_name}] ({self.id}) {str(self.__dict__)}"
+
+
+    def save(self):
+        """
+        updates the public instance attribute updated_at
+        with the current datetime
+        """
+        self.updated_at = datetime.now()
+
+
+    def to_dict(self):
+        """
+        returns a dictionary containing
+        all keys/values of __dict__ of the instance
+        """
+        dictionary = self.__dict__.copy()
+        dictionary['__class__'] = self.__class__.__name__
+        dictionary['created_at'] = self.created_at.isoformat()
+        dictionary['updated_at'] = self.updated_at.isoformat()
+        return dictionary
