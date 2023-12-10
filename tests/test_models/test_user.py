87c2221ae1723603967de175228cf64b008a2ca4
K.A. Osei-Sarfo <astrokojo@gmail.com>
2023-12-10 20:51:11 +0000

    added: user class unit test

---
+++ tests/test_models/test_user.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+Unit Test Module for the User class
+"""
+
+import unittest 
+from models.user import User
+from models.base_model import BaseModel
+
+class TestUSer(unittest.TestCase):
+    """Test Cases for the USer Class."""
+    
+    def setUp(self):
+        pass
+    
+    def tearDown(self):
+        pass
+    
+    def test_instantiation(self):
+        """Tests instantiation of USer Class"""
+        user = User()
+        self.assertIsInstance(user, User)
+    
+    def test_user_inheritance(self):
+        """Test USer inheritance from BaseModel""" 
+        user = User()
+        self.assertTrue(issubclass(User, BaseModel))
